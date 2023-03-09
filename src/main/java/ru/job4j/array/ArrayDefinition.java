df8d061645412850dd33f2601338fc682089b7c4
michaelberezovski <berezovskii.mikhail@gmail.com>
2023-03-09 15:35:41 +0300

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,14 @@ public class ArrayDefinition {
         System.out.println(ages.length);
         System.out.println(surnames.length);
         System.out.println(prices.length);
+        String[] names = new String[4];
+        names[0] = "John";
+        names[1] = "Mike";
+        names[2] = "Bill";
+        names[3] = "Helen";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
